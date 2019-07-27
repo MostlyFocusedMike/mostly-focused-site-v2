867ac46dc58223f3db429fe61d43b44a5b70995c
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-07-27 12:13:01 -0400

    move articlesconverter to own folder

---
+++ src/ArticlesConverter/index.js
@@ -0,0 +1,49 @@
+class ArticlesConverter {
+    constructor(rawText, userHandle) {
+        this.userHandle = userHandle;
+        this.rawText = rawText;
+        return this.articles;
+    }
+
+    checkForImage = (imageId) => (
+        imageId
+            ? `https://miro.medium.com/max/1400/${imageId}`
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Pictogram_voting_question.svg/440px-Pictogram_voting_question.svg.png'
+    );
+
+    formatTags = (tags) => tags.map(({ name, slug }) => ({ name, slug }))
+
+    formatArticle = (rawArticle) => {
+        const {
+            id,
+            title,
+            slug,
+            uniqueSlug,
+            virtuals: { previewImage: { imageId }, tags },
+            content: { subtitle, metaDescription },
+        } = rawArticle;
+
+        const link = `https://medium.com/@${this.userHandle}/${uniqueSlug}`;
+
+        return {
+            id,
+            title,
+            slug,
+            link,
+            image: this.checkForImage(imageId),
+            subtitle,
+            metaDescription,
+            tags: this.formatTags(tags),
+        };
+    };
+
+    get jsonArticles() {
+        return JSON.parse(this.rawText.slice(this.rawText.indexOf('{'))).payload.references.Post;
+    }
+
+    get articles() {
+        return Object.keys(this.jsonArticles).map(article => this.formatArticle(this.jsonArticles[article]));
+    }
+}
+
+export default ArticlesConverter;
