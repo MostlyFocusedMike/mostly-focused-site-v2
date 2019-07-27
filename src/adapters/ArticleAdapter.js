4f8fc6316ecb5169068b336fbc8f253b4c29e15c
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-07-27 00:44:41 -0400

    add tags to articles

--- src/adapters/ArticleAdapter.js
+++ src/adapters/ArticleAdapter.js
@@ -1,20 +1,44 @@
+/* eslint-disable object-curly-newline */
 import articlesRaw from './articles';
+
+const imageBase = 'https://miro.medium.com/max/1400/';
+
+const formatTags = (tags) => {
+    return tags.map(({ name, slug, postCount, metadata: { coverImage } }) => {
+        const image = coverImage.id ? `${imageBase}${coverImage.id}` : undefined;
+        return { name, slug, image, postCount };
+    });
+}
+
+const formatArticle = (post) => {
+    const {
+        id,
+        title,
+        slug,
+        uniqueSlug,
+        virtuals: { previewImage: { imageId }, tags },
+        content: { subtitle, metaDescription },
+    } = post;
+    const image = imageId ? `${imageBase}${imageId}` : undefined;
+
+    return {
+        id,
+        title,
+        slug,
+        uniqueSlug,
+        image,
+        subtitle,
+        metaDescription,
+        tags: formatTags(tags),
+    };
+};
 
 class ArticleAdapter {
     static getAll() {
         const posts = articlesRaw.payload.references.Post;
         const articles = [];
         Object.keys(posts).forEach((post) => {
-            const article = posts[post];
-            articles.push({
-                id: article.id,
-                title: article.title,
-                slug: article.slug,
-                uniqueSlug: article.uniqueSlug,
-                image: `https://miro.medium.com/max/1400/${article.virtuals.previewImage.imageId}`,
-                subtitle: article.content.subtitle,
-                metaDescription: article.content.metaDescription,
-            });
+            articles.push(formatArticle(posts[post]));
         });
 
         return Promise.resolve(articles);
