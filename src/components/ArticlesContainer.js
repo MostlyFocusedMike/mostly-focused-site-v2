930b520b31cbff4f5d778ca42d1f062e11e92397
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-08-04 11:49:48 -0400

    rough render articles

--- src/components/ArticlesContainer.js
+++ src/components/ArticlesContainer.js
@@ -2,11 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { ArticleAdapter } from '../Adapters';
 
 const ArticlesContainer = () => {
-    ArticleAdapter.getAll()
-        .then(console.log);
+    const [articles, setArticles] = useState([
+        {
+            mediumID: '',
+            title: '',
+            slug: '',
+            link: '',
+            image: '',
+            subtitle: '',
+            tags: [],
+        },
+    ]);
+
+    useEffect(() => {
+        ArticleAdapter.getAll()
+            .then((jsonArticles) => {
+                setArticles(jsonArticles);
+            });
+    }, []);
+
     return (
-        <div id="articles-container">
+        <div id='articles-container'>
             <h1>My Articles</h1>
+            {
+                articles.map(article => {
+                    return <p key={article.mediumID}>{article.title}</p>;
+                })
+            }
         </div>
     );
 };
