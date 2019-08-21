6f94242e70d42c855e52bb9251bc2e08dec6bea8
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-08-20 23:56:25 -0400

    refactor tag adapter

--- src/Adapters/TagAdapter.js
+++ src/Adapters/TagAdapter.js
@@ -1,23 +1,28 @@
+const filterTags = (tag, tags) => {
+    if (tags.findIndex(x => x.slug === tag.slug) === -1) {
+        tags.push(tag);
+    }
+};
+
+const sortTags = (a, b) => {
+    if (a.slug > b.slug) return 1;
+    if (a.slug < b.slug) return -1;
+    return 0;
+};
+
+const getAllTags = (articles) => {
+    const result = [];
+    articles.forEach(article => {
+        article.tags.forEach(tag => filterTags(tag, result));
+    });
+    return result;
+};
+
 class TagAdapter {
     static getAll() {
         return fetch(this.url, this.options)
             .then(r => r.json())
-            .then(articles => {
-                const tags = [];
-                articles.forEach(article => {
-                    article.tags.forEach(tag => {
-                        if (tags.findIndex(x => x.slug === tag.slug) === -1) {
-                            tags.push(tag);
-                        }
-                    });
-                });
-                tags.sort((a, b) => {
-                    if (a.slug > b.slug) return 1;
-                    if (a.slug < b.slug) return -1;
-                    return 0;
-                });
-                return tags;
-            });
+            .then(articles => getAllTags(articles).sort(sortTags));
     }
 }
 
