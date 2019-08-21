128b2d36940f4d7350a9bd10fa4166ec1c16d30f
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-08-20 23:17:48 -0400

    add search bar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ArticleAdapter } from './Adapters';
 import './App.css';
 import ArticlesContainer from './components/Articles/ArticlesContainer';
+import SearchBar from './components/SearchBar';
 
 function App() {
     const [articles, setArticles] = useState([
@@ -25,27 +26,7 @@ function App() {
 
     return (
         <div className='App'>
-            <form action="post">
-                <div id="select-holder">
-                    <label htmlFor="sorting-buttons">find articles about: </label>
-                    <select id="sorting-buttons" >
-                        <option value="all">Anything</option>
-                        <option value="ruby">Ruby</option>
-                        <option value="js">Javascript</option>
-                        <option value="databases">Databases</option>
-                    </select>
-                    <div id="fake-border">
-                        <br />
-                    </div>
-                    <div id="arrows-holder">
-                        <div id="arrows">
-                            <br />
-                        </div>
-                    </div>
-                </div>
-                <br />
-                <input type="submit" name="" id="" value="Sort Articles" />
-            </form>
+            <SearchBar />
             <ArticlesContainer articles={articles}/>
         </div>
     );
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const SearchBar = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('e.target.value: ', e.target.value);
+    };
+    // control form
+    // grab state
+    // set context filter value
+    // make tags adapter to grab possible values
+    // filter the value in the articles component
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div id="select-holder">
+                <label htmlFor="sorting-buttons">find articles about: </label>
+                <select id="sorting-buttons" >
+                    <option value="all">Anything</option>
+                    <option value="ruby">Ruby</option>
+                    <option value="js">Javascript</option>
+                    <option value="databases">Databases</option>
+                </select>
+                <div id="fake-border">
+                    <br />
+                </div>
+                <div id="arrows-holder">
+                    <div id="arrows">
+                        <br />
+                    </div>
+                </div>
+            </div>
+            <br />
+            <input type="submit" name="" id="" value="Sort Articles" />
+        </form>
+    )
+};
+
+export default SearchBar;
