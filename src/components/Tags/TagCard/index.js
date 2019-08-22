d89505d3ee62a89c117720c0f393c81f074e2b8c
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-08-22 00:07:58 -0400

    add filtering to tag buttons

--- src/components/Tags/TagCard/index.js
+++ src/components/Tags/TagCard/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import appContext from '../../../context';
 
 const TagCard = ({ tag }) => {
+    const { setChosenTag } = useContext(appContext);
+
+    const handleClick = (e) => {
+        setChosenTag(e.target.dataset.value);
+    };
+
     return (
         <li className='tag-li'>
-            <input type='button' value={tag.name} />
+            <input
+                type='button'
+                value={tag.name}
+                data-value={tag.slug}
+                onClick={handleClick}
+            />
         </li>
     );
 };
