ff7ddad565c14bb904108cff693a728fdbba3679
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-08-21 23:59:07 -0400

    filter articles

--- src/components/Articles/ArticlesContainer.js
+++ src/components/Articles/ArticlesContainer.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext }  from 'react';
 import PropTypes from 'prop-types';
 import ArticleCard from './ArticleCard';
+import appContext from '../../context';
 
 const ArticlesContainer = ({ articles }) => {
+    const { chosenTag } = useContext(appContext);
+    const filteredArticles = articles.filter((article) => {
+        if (chosenTag === 'anything') return true;
+        if (article.tags.findIndex(x => x.slug === chosenTag) > -1) return true;
+    });
+
     return (
         <div id='articles-div'>
             <h1>My Articles</h1>
             <ul id='articles-ul'>
                 {
-                    articles.map((article) => (
+                    filteredArticles.map((article) => (
                         <ArticleCard
                             article={article}
                             key={article.mediumID}
