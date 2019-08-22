1bb8ee8a9e96908ca55461e03712ace17f63a3dd
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-08-22 00:01:46 -0400

    refactor

--- src/components/Articles/ArticlesContainer.js
+++ src/components/Articles/ArticlesContainer.js
@@ -1,14 +1,13 @@
-import React, { useContext }  from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ArticleCard from './ArticleCard';
 import appContext from '../../context';
 
 const ArticlesContainer = ({ articles }) => {
     const { chosenTag } = useContext(appContext);
-    const filteredArticles = articles.filter((article) => {
-        if (chosenTag === 'anything') return true;
-        if (article.tags.findIndex(x => x.slug === chosenTag) > -1) return true;
-    });
+    const filteredArticles = articles.filter((article) => (
+        (chosenTag === 'anything') || (article.tags.findIndex(x => x.slug === chosenTag) > -1)
+    ));
 
     return (
         <div id='articles-div'>
