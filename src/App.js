119ade2411d1ced62aa9360ecd63266abebd3013
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-11-15 20:42:10 -0500

    add navbar in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Routes from './Routes';
+import NavBar from './components/NavBar';
 
 function App() {
     return (
         <div className='App'>
+            <NavBar />
             <Routes />
         </div>
     );
